7b0cd2096dad6814a5680b1e3115e71c72e5d1a4
Kaique Silva Sousa <ksilvasousa1@gmail.com>
2025-07-20 11:40:32 -0300

    Ultima aula

---
+++ Aulas/aula_14-07/Arborizavel.java
@@ -0,0 +1,12 @@
+
+public interface Arborizavel<T> {
+
+    NoTriplo<T> getRaiz();
+    void inserir(T dado);
+    T apagar(T dado);
+    boolean existe(T dado);
+    void limpar();
+    String imprimirPreOrdem();
+    String imprimirEmOrdem();
+    String imprimirPosOrdem();
+}
